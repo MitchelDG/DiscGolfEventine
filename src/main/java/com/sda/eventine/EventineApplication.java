e7c6fb822d161bd9992d3038896235568054b72b
Michal Kober <borek.michla@email.cz>
2021-10-24 10:44:47 +0200

    Initial commit

---
+++ src/main/java/com/sda/eventine/EventineApplication.java
@@ -0,0 +1,13 @@
+package com.sda.eventine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/eventine/EventineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.eventine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
